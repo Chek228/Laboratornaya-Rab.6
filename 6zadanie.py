f4ea2c9add22a0d329d8c3f2e9afe3b46f5231de
Dju <chekowchek@gmail.com>
2022-06-02 23:07:41 +0300

    1

--- 6zadanie.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-if __name__ == '__main__':
-    for a in [(1, 1, 1), (1, 2, 2), (1, 1, 2, 2), ()]:
-            c = len(list(filter(lambda x: x == a[0], a)))
-            print(c)
-            print(a[c:])
-            print()
